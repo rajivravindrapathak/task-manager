47fbc69079d00a8a41cf69f5bf35443b4ad4946e
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-09-10 17:03:52 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+const cors = require('cors');
+const app = express();
 
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse JSON bodies
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/taskdb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err));
+
+// Task routes
+app.use('/tasks', taskRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
